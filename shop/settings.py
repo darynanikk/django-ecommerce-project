a49642904716637b7106dca6682e87c44a0b3f7b
daryna <darynadobro@gmail.com>
2022-08-19 22:31:55 +0200

    configure order middleware

--- shop/settings.py
+++ shop/settings.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "store.middleware.CountOrderedItemsMiddleware",
+    "store.middleware.OrderMiddleware",
 ]
 
 ROOT_URLCONF = "shop.urls"
--- store/middleware.py
+++ store/middleware.py
@@ -4,12 +4,10 @@ from customer.models import Customer
 from store.models import Order
 
 
-class CountOrderedItemsMiddleware(MiddlewareMixin):
+class OrderMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         try:
-            customer = Customer.objects.get(user=request.user)
-            request.customer = customer
-            request.order = Order.objects.get(customer=customer)
+            request.order = Order.objects.get(customer=request.user)
         except:
             pass
