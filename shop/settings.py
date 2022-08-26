027a01ac588237ee2fd44e49d0a5417cc38b22c2
daryna <darynadobro@gmail.com>
2022-08-26 22:47:37 +0200

    configure settings

--- shop/settings.py
+++ shop/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DEBUG")) == "1"
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", 'dn-ecommerce-project.herokuapp.com']
 
