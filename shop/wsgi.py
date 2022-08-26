056478216bb41c1e410ed1942c62ec17da24316c
daryna <darynadobro@gmail.com>
2022-08-26 22:22:34 +0200

    set wsgi to defaults

--- shop/wsgi.py
+++ shop/wsgi.py
@@ -1,26 +1,16 @@
 """
-WSGI config for shop project.
+WSGI config for apiary_maintenance_service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-import pathlib
-
-import dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-CURRENT_DIR = pathlib.Path(__file__).resolve().parent
-BASE_DIR = CURRENT_DIR.parent
-ENV_FILE_PATH = BASE_DIR / ".env"
-
-dotenv.load_dotenv(str(ENV_FILE_PATH))
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apiary_maintenance_service.settings')
 
 application = get_wsgi_application()
